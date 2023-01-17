67f8f2f9d8df74e670246b2ef8b2f5b2987c4a3e
Willian Wang <git@willian.wang>
2023-01-16 22:33:11 -0300

    Improve code formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use uint::construct_uint;
 use std::str::FromStr;
+use uint::construct_uint;
 use wasm_bindgen::prelude::*;
 
 construct_uint! {
@@ -25,30 +25,75 @@ pub struct Key {
 }
 
 const G: Point = Point {
-    x: U512([0x343280d6115c1d21, 0x4a03c1d356c21122, 0x6bb4bf7f321390b9, 0xb70e0cbd, 0x0, 0x0, 0x0, 0x0]),
-    y: U512([0x44d5819985007e34, 0xcd4375a05a074764, 0xb5f723fb4c22dfe6, 0xbd376388, 0x0, 0x0, 0x0, 0x0]),
+    x: U512([
+        0x343280d6115c1d21,
+        0x4a03c1d356c21122,
+        0x6bb4bf7f321390b9,
+        0xb70e0cbd,
+        0x0,
+        0x0,
+        0x0,
+        0x0,
+    ]),
+    y: U512([
+        0x44d5819985007e34,
+        0xcd4375a05a074764,
+        0xb5f723fb4c22dfe6,
+        0xbd376388,
+        0x0,
+        0x0,
+        0x0,
+        0x0,
+    ]),
 };
-const P: U512 = U512([0x0000000000000001, 0xffffffff00000000, 0xffffffffffffffff, 0xffffffff, 0x0, 0x0, 0x0, 0x0]);
-const A: U512 = U512([0xfffffffffffffffe, 0xfffffffeffffffff, 0xffffffffffffffff, 0xffffffff, 0x0, 0x0, 0x0, 0x0]);
-const N: U512 = U512([0x13dd29455c5c2a3d, 0xffff16a2e0b8f03e, 0xffffffffffffffff, 0xffffffff, 0x0, 0x0, 0x0, 0x0]);
+const P: U512 = U512([
+    0x0000000000000001,
+    0xffffffff00000000,
+    0xffffffffffffffff,
+    0xffffffff,
+    0x0,
+    0x0,
+    0x0,
+    0x0,
+]);
+const A: U512 = U512([
+    0xfffffffffffffffe,
+    0xfffffffeffffffff,
+    0xffffffffffffffff,
+    0xffffffff,
+    0x0,
+    0x0,
+    0x0,
+    0x0,
+]);
+const N: U512 = U512([
+    0x13dd29455c5c2a3d,
+    0xffff16a2e0b8f03e,
+    0xffffffffffffffff,
+    0xffffffff,
+    0x0,
+    0x0,
+    0x0,
+    0x0,
+]);
 
 const KEY_SIZE: usize = 224;
 const WINDOW_SIZE: usize = 5;
 
 impl U512 {
     fn inv_mod(self, m: U512) -> U512 {
-        let (mut r, mut new_r) = (self%m, m);
+        let (mut r, mut new_r) = (self % m, m);
         if r.is_zero() {
             panic!("a is not invertible");
         }
         let (mut t, mut new_t) = (U512::one(), U512::zero());
-        while ! new_r.is_zero() {
+        while !new_r.is_zero() {
             let quotient = r / new_r;
             (r, new_r) = (new_r, r - quotient * new_r);
             (t, new_t) = (new_t, t + (m - quotient % m) * new_t);
-            new_t = new_t % m;
+            new_t %= m;
         }
-        return t;
+        t
     }
 }
 
@@ -57,34 +102,34 @@ impl JacobianPoint {
         let z_inv = self.z.inv_mod(P);
         let z_inv2 = (z_inv * z_inv) % P;
         let z_inv3 = (z_inv2 * z_inv) % P;
-        return Point {
+        Point {
             x: (self.x * z_inv2) % P,
             y: (self.y * z_inv3) % P,
-        };
+        }
     }
 
     fn zero() -> JacobianPoint {
-        return JacobianPoint {
+        JacobianPoint {
             x: U512::zero(),
             y: U512::zero(),
             z: U512::one(),
-        };
+        }
     }
 
     fn negate(&self) -> JacobianPoint {
-        return JacobianPoint {
+        JacobianPoint {
             x: self.x,
             y: P - self.y,
             z: self.z,
-        };
+        }
     }
 
     fn add(&self, other: &JacobianPoint) -> JacobianPoint {
         if self.x.is_zero() && self.y.is_zero() {
-            return other.clone();
+            return *other;
         }
         if other.x.is_zero() && other.y.is_zero() {
-            return self.clone();
+            return *self;
         }
         // https://hyperelliptic.org/EFD/g1p/auto-shortw-jacobian.html#addition-add-1998-cmo-2
         let z1z1 = (self.z * self.z) % P;
@@ -99,15 +144,19 @@ impl JacobianPoint {
         if u1 == u2 && s1 != s2 {
             return JacobianPoint::zero();
         }
-        let h = (u2 + (P-u1)) % P;
+        let h = (u2 + (P - u1)) % P;
         let hh = (h * h) % P;
         let hhh = (hh * h) % P;
-        let r = (s2 + (P-s1)) % P;
+        let r = (s2 + (P - s1)) % P;
         let v = (u1 * hh) % P;
-        let x3 = ((r * r) + (P-hhh) + (P-v)*2) % P;
-        let y3 = (r * (v + (P-x3)) + (P-s1) * hhh) % P;
+        let x3 = ((r * r) + (P - hhh) + (P - v) * 2) % P;
+        let y3 = (r * (v + (P - x3)) + (P - s1) * hhh) % P;
         let z3 = (h * ((self.z * other.z) % P)) % P;
-        return JacobianPoint { x: x3, y: y3, z: z3 };
+        JacobianPoint {
+            x: x3,
+            y: y3,
+            z: z3,
+        }
     }
 
     fn double(&self) -> JacobianPoint {
@@ -120,49 +169,54 @@ impl JacobianPoint {
         let yyyy = (yy * yy) % P;
         let zz = (self.z * self.z) % P;
         let tmp_s = (self.x + yy) % P;
-        let s = (((tmp_s*tmp_s) % P + (P-xx) + (P-yyyy))*2) % P;
-        let m = (xx*3 + A * ((zz*zz)%P)) % P;
-        let t = ((m*m) + (P-s)*2) % P;
+        let s = (((tmp_s * tmp_s) % P + (P - xx) + (P - yyyy)) * 2) % P;
+        let m = (xx * 3 + A * ((zz * zz) % P)) % P;
+        let t = ((m * m) + (P - s) * 2) % P;
         let x3 = t % P;
-        let y3 = (m*(s + (P-t)) + (P-yyyy)*8) % P;
+        let y3 = (m * (s + (P - t)) + (P - yyyy) * 8) % P;
         let tmp_z3 = self.z + self.y;
-        let z3 = ((tmp_z3*tmp_z3) + (P-yy) + (P-zz)) % P;
-        return JacobianPoint { x: x3, y: y3, z: z3 };
+        let z3 = ((tmp_z3 * tmp_z3) + (P - yy) + (P - zz)) % P;
+        JacobianPoint {
+            x: x3,
+            y: y3,
+            z: z3,
+        }
     }
 
     fn mul(&self, other: U512) -> JacobianPoint {
         let mut result = JacobianPoint::zero();
         let mut multiplier = other % N;
         let addend = self;
-        
-        let mut precomp : [JacobianPoint; (1 << (WINDOW_SIZE-1))] = [JacobianPoint::zero(); (1 << (WINDOW_SIZE-1))];
-        for i in 1..(1 << (WINDOW_SIZE-1)) {
-            precomp[i] = precomp[i-1].add(&addend);
+
+        let mut precomp: [JacobianPoint; (1 << (WINDOW_SIZE - 1))] =
+            [JacobianPoint::zero(); (1 << (WINDOW_SIZE - 1))];
+        for i in 1..(1 << (WINDOW_SIZE - 1)) {
+            precomp[i] = precomp[i - 1].add(addend);
         }
 
-        let mut naf : [i32; KEY_SIZE+1] = [0; KEY_SIZE+1];
+        let mut naf: [i32; KEY_SIZE + 1] = [0; KEY_SIZE + 1];
         let mask = (U512::one() << WINDOW_SIZE) - 1;
-        for i in 0..KEY_SIZE+1 {
-            if !(multiplier & U512::one()).is_zero() { 
+        for i in 0..naf.len() {
+            if !(multiplier & U512::one()).is_zero() {
                 naf[i] = (multiplier & mask).as_u32() as i32;
-                if naf[i] >= (1 << (WINDOW_SIZE-1)) {
+                if naf[i] >= (1 << (WINDOW_SIZE - 1)) {
                     naf[i] -= 1 << WINDOW_SIZE;
                     multiplier = multiplier + (-naf[i]);
                 } else {
                     multiplier = multiplier - naf[i];
                 }
             };
-            multiplier = multiplier >> 1;
+            multiplier >>= 1;
             if multiplier.is_zero() {
                 break;
             }
         }
-        for i in (0..KEY_SIZE+1).rev() {
+        for i in (0..KEY_SIZE + 1).rev() {
             result = result.double();
             if naf[i] == 0 {
                 continue;
             }
-            let ind = naf[i].abs() as usize;
+            let ind = naf[i].unsigned_abs() as usize;
             if naf[i] > 0 {
                 result = result.add(&precomp[ind]);
             } else {
@@ -170,21 +224,20 @@ impl JacobianPoint {
             }
         }
 
-        return result;
+        result
     }
-
 }
 
 impl Point {
     fn to_jacobian(&self) -> JacobianPoint {
-        return JacobianPoint {
+        JacobianPoint {
             x: self.x,
             y: self.y,
             z: U512::one(),
-        };
+        }
     }
     fn mul(&self, other: U512) -> Point {
-        return self.to_jacobian().mul(other).to_affine();
+        self.to_jacobian().mul(other).to_affine()
     }
 }
 
@@ -193,42 +246,42 @@ impl Key {
     pub fn from_private_hex(s: &str) -> Key {
         let k = U512::from_str(s).unwrap();
         let p = G.mul(k);
-        return Key {
+        Key {
             private: k,
             public: p,
-        };
+        }
     }
     pub fn from_public_hex(s: &str) -> Key {
-        if s.len() == (KEY_SIZE/2 + 1) {
-            let xy = U512::from_str(s).unwrap() ^ (U512::one() << (2*KEY_SIZE+2));
+        if s.len() == (KEY_SIZE / 2 + 1) {
+            let xy = U512::from_str(s).unwrap() ^ (U512::one() << (2 * KEY_SIZE + 2));
             let x = xy >> KEY_SIZE;
             let y = xy & ((U512::one() << KEY_SIZE) - 1);
-            return Key {
+            Key {
                 private: U512::zero(),
-                public: Point {
-                    x: x,
-                    y: y,
-                },
-            };
-        } else if s.len() == (KEY_SIZE/4 + 1) {
+                public: Point { x, y },
+            }
+        } else if s.len() == (KEY_SIZE / 4 + 1) {
             panic!("compressed keys not supported yet");
         } else {
             panic!("Invalid public key length");
         }
     }
     pub fn get_private_hex(&self) -> String {
-        return format!("{:0>64x}", self.private);
+        format!("{:0>64x}", self.private)
     }
     pub fn get_public_hex(&self) -> String {
-        return format!("{:0>64x}", (U512::one() << (2*KEY_SIZE+2)) | (self.public.x << KEY_SIZE) | self.public.y);
+        format!(
+            "{:0>64x}",
+            (U512::one() << (2 * KEY_SIZE + 2)) | (self.public.x << KEY_SIZE) | self.public.y
+        )
     }
     pub fn derive(&self, other: &Key) -> Key {
-        if ! self.private.is_zero() {
-            return Key {
+        if !self.private.is_zero() {
+            Key {
                 private: U512::zero(),
                 public: other.public.mul(self.private),
-            };
-        } else if ! other.private.is_zero() {
+            }
+        } else if !other.private.is_zero() {
             return Key {
                 private: U512::zero(),
                 public: self.public.mul(other.private),
--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,14 @@ fn main() {
     if args.len() <= 1 || args.len() > 3 {
         println!("Usage: {} <private key> [public key]", args[0]);
         println!("Examples:");
-        println!("  {} af0181e90420508e39e9d862f1680dc22f5f024fcfc11939d7c6fedb", args[0]);
-        println!("  {} 14447aec7d78c691d0c1c94da1a6a85d9eefeddf8b42f51aa227376c", args[0]);
+        println!(
+            "  {} af0181e90420508e39e9d862f1680dc22f5f024fcfc11939d7c6fedb",
+            args[0]
+        );
+        println!(
+            "  {} 14447aec7d78c691d0c1c94da1a6a85d9eefeddf8b42f51aa227376c",
+            args[0]
+        );
         println!("  {} af0181e90420508e39e9d862f1680dc22f5f024fcfc11939d7c6fedb 457e0910e4a34933c1ad3034ad92504c8324b8701e56c37716bf541967813d3ff1390e5c1d0c833f1fce3ff8bc69b277a072c3c31b239aacf", args[0]);
         println!("  {} 14447aec7d78c691d0c1c94da1a6a85d9eefeddf8b42f51aa227376c 4bcf74addac6c83a587eeb6d2a724158cebaecfed0af82a90434268e03c82f21e137c7341a70c0044ed058d5fe6c7aa38eb16542fdf5ae111", args[0]);
         return;
